c2f0f5ad0094e5bb56dd5df5da9135de24844037
AcoaAera <korablewaleksei1997@yandex.ru>
2020-12-11 01:29:13 +0500

    docker-compose

---
+++ Lab_5/server/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+// константы
+const port = 8080;
+const host = '0.0.0.0';
+let counter = 0;
+// приложение
+const app = express();
+app.get('/', (req, res) => {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  counter += 1;
+  res.end(`<h1>${host}:${port} visit cointer is ${counter}</h1>`);
+});
+
+app.listen(port, host);
+console.log(`running on http://${host}:${port}`);
